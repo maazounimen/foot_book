caef2f93be51f4abc296c72935a371f82435f62e
imen <imen@imen-PC>
2014-11-24 19:25:06 +0100

    adding the model class User
    Signed-off-by: imen <imen@imen-PC>

---
+++ tn.edu.esprit.cs.foot_book/src/tn/edu/esprit/cs/foot_book/model/User.java
@@ -0,0 +1,38 @@
+package tn.edu.esprit.cs.foot_book.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+
+	public User() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public User(String name) {
+		super();
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
