e45fada338cfc6c157a6eba884073eea4feed0cd
imen <imen@172.16.1.50>
2014-12-13 15:04:54 +0100

    ejb project whith gitignore on the eclipse configurations files.
    Signed-off-by: imen <imen@172.16.1.50>

---
+++ tn.edu.esprit.cs.foot_book_ejb/ejbModule/tn/edu/esprit/cs/foot_book_ejb/model/User.java
@@ -0,0 +1,38 @@
+package tn.edu.esprit.cs.foot_book_ejb.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private int id;
+	private String name;
+
+	public User() {
+	}
+
+	@Id
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
